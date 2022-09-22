c8c46edc1d624571067be958647a777407018b1e
pilgyu14 <pilgue0822@naver.com>
2022-09-22 19:39:13 +0900

    0922_input스크립트 수정

--- Assets/PG/InputSystem/InputTest.cs
+++ Assets/PG/InputSystem/InputTest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem; 
 
-public class InputSystem : MonoBehaviour
+public class InputTest : MonoBehaviour
 {
     public void OnDrag()
     {
