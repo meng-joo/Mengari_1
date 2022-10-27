e5d0f17402378cdee11a771dd56d9e96c66a3f73
Daehee Kang <daeheuigang@gmail.com>
2022-10-27 20:29:16 +0900

    게임오버씬 작업중

--- Assets/Scripts/DaeheeUIGameOver.cs
+++ Assets/Scripts/DaeheeUIGameOver.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class DaeheeUIGameOver : MonoBehaviour
 {
@@ -14,13 +15,25 @@ public class DaeheeUIGameOver : MonoBehaviour
     public TextMeshProUGUI titleText;
     public RectTransform restartText;
 
+
+    public RectTransform settingPanel;
+    public RectTransform quitBtnTransform;
+    public RectTransform volumeBtnTransform;
+
     void Awake()
     {
+        _seq = DOTween.Sequence();
         foreach (Image image in btnImageList)
         {
             _seq.Join(image.DOFade(0, 0f));
         }
-        titleText.DOFade(0, 0f);
+        _seq.Join(titleText.DOFade(0, 0f));
+        _seq.Join(settingPanel.DOAnchorPosX(-2000f, 2f));
+        _seq.OnComplete(() =>
+        {
+            _seq.Kill();
+        });
+
     }
 
     void Start()
@@ -45,4 +58,69 @@ public class DaeheeUIGameOver : MonoBehaviour
             .Append(restartText.DOScale(new Vector3(1f, 1f, 1f), 2f)).SetLoops(-1, LoopType.Restart);
     }
 
+
+    public void SettingBtn()
+    {
+        _seq.Append(settingPanel.DOAnchorPosX(720, 1f).SetEase(Ease.OutBounce));
+
+        _seq.AppendCallback(() =>
+        {
+            _seq.Kill();
+        });
+
+    }
+
+    #region QuitBtn
+    public void ClickDownQuitBtn()
+    {
+        _seq = DOTween.Sequence();
+        Debug.Log("Down");
+        _seq.Join(quitBtnTransform.DOScale(new Vector3(1.5f, 1.5f, 1.5f), .3f).SetEase(Ease.OutBounce));
+        _seq.AppendCallback(() =>
+        {
+            _seq.Kill();
+        });
+    }
+
+    public void ClickUpQuitBtn()
+    {
+        _seq = DOTween.Sequence();
+        Debug.Log("Up");
+        _seq.Join(quitBtnTransform.DOScale(new Vector3(1f, 1f, 1f), .3f).SetEase(Ease.OutBounce));
+        _seq.AppendCallback(() =>
+        {
+            _seq.Kill();
+        });
+    }
+
+    #endregion
+
+
+    #region VolumeBtn
+    public void ClickUpVolumeBtn()
+    {
+        _seq = DOTween.Sequence();
+        Debug.Log("Up");
+        _seq.Join(volumeBtnTransform.DOScale(new Vector3(4f, 4f, 4f), .3f).SetEase(Ease.OutBounce));
+        _seq.AppendCallback(() =>
+        {
+            _seq.Kill();
+        });
+    }
+
+    public void ClickDownVolumeBtn()
+    {
+        _seq = DOTween.Sequence();
+        Debug.Log("Up");
+        _seq.Join(volumeBtnTransform.DOScale(new Vector3(3f, 3f, 3f), .3f).SetEase(Ease.OutBounce));
+        _seq.AppendCallback(() =>
+        {
+            _seq.Kill();
+        });
+    }
+    #endregion
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
