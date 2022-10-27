ed7cbbed5c62539d43bec8f486860cfa63baff84
HyunjunDev <101493216+HyunjunDev@users.noreply.github.com>
2022-10-27 20:19:40 +0900

    UI수정

--- Assets/Scripts/UI/InGameUIManager.cs
+++ Assets/Scripts/UI/InGameUIManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using DG.Tweening;
+using Cinemachine;
 using System;
 
 public class InGameUIManager : MonoBehaviour
@@ -17,17 +18,23 @@ public class InGameUIManager : MonoBehaviour
     public Text levelText;
 
     private Sequence _seq;
+    public CinemachineVirtualCamera inGameVCam;
 
     public void OnClickStartButton()
     {
         _seq = DOTween.Sequence();
+
         _seq.Append(startButton.transform.DOScale(0f, 0.15f));
-        _seq.Join(levelText.transform.DOMoveY(2430f, 0.3f));
-        _seq.Join(cardPanel.transform.DOMoveY(500f, 0.3f));
+        _seq.AppendCallback(() => startButton.gameObject.SetActive(false));
+
+        _seq.AppendCallback(() => inGameVCam.Priority = 20);
+        _seq.AppendInterval(1.4f);
+
+        _seq.Append(levelText.transform.DOMoveY(2430f, 0.8f));
+        _seq.Join(cardPanel.transform.DOMoveY(500f, 1.2f));
         for (int i = 0; i < levelImages.Length; i++)
         {
-            _seq.Append(levelImages[i].transform.DOMoveY(2320f, 0.15f).SetEase(Ease.OutBack));
+            _seq.Append(levelImages[i].transform.DOMoveY(2320f, 0.25f).SetEase(Ease.OutBack));
         }
-        startButton.gameObject.SetActive(false);
     }
 }
