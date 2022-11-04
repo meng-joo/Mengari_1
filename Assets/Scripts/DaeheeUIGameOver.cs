db36fb6252138995b49f2cdb727561d02b95526c
Daehee Kang <daeheuigang@gmail.com>
2022-11-04 23:10:20 +0900

    사운드 작업중

--- Assets/Scripts/DaeheeUIGameOver.cs
+++ Assets/Scripts/DaeheeUIGameOver.cs
@@ -5,6 +5,7 @@ using DG.Tweening;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+using Unity.XR.OpenVR;
 
 public class DaeheeUIGameOver : MonoBehaviour
 {
@@ -18,14 +19,21 @@ public class DaeheeUIGameOver : MonoBehaviour
 
 	public RectTransform settingPanel;
 	public RectTransform quitBtnTransform;
-	public RectTransform volumeBtnTransform;
+	public RectTransform effectVolumeSound;
+	public RectTransform bgmVolumeSound;
 
 	public Image backgroundImg;
 
 
+	#region Vive Valuable
+	public GameObject vibeObject;
+	public List<Image> VibeImages;
+	private int _vibeIndex = 0;
+	#endregion
 
+	public AudioClip uiAudioClip;
 
-	void Awake()
+    void Awake()
 	{
 		_seq = DOTween.Sequence();
 		foreach (Image image in btnImageList)
@@ -64,16 +72,19 @@ public class DaeheeUIGameOver : MonoBehaviour
 		_restartTextSeq = DOTween.Sequence();
 		_restartTextSeq.Append(restartText.DOScale(new Vector3(1.2f, 1.2f, 1.2f), 1f))
 			.Append(restartText.DOScale(new Vector3(1f, 1f, 1f), 2f)).SetLoops(-1, LoopType.Restart);
-	}
+        vibeObject.GetComponent<Image>().sprite = VibeImages[0].sprite;
+
+    }
 
 
-	public void SettingBtn()
+    public void SettingBtn()
 	{
+		SoundManager.instance.SFXPlay("ui", uiAudioClip);
         _seq = DOTween.Sequence();
         _seq.Append(settingPanel.DOAnchorPosX(0, 1f).SetEase(Ease.OutBounce));
 		_seq.Join(settingPanel.DOScale(new Vector3(1, 1, 1), 0.7f));
 		backgroundImg.enabled = true;
-		_seq.Join(backgroundImg.DOFade(0.5f, 0.7f)).OnComplete(()=>
+		_seq.Join(backgroundImg.DOFade(0.8f, 0.7f)).OnComplete(()=>
 		{
 			_seq.Kill();
 		});
@@ -82,54 +93,96 @@ public class DaeheeUIGameOver : MonoBehaviour
 	#region QuitBtn
 	public void ClickDownQuitBtn()
 	{
-		_seq = DOTween.Sequence();
+        SoundManager.instance.SFXPlay("ui", uiAudioClip);
+
+        _seq = DOTween.Sequence();
 		Debug.Log("Down");
 		_seq.Join(quitBtnTransform.DOScale(new Vector3(1.5f, 1.5f, 1.5f), .3f).SetEase(Ease.OutBounce));
-
-	}
+        _seq.AppendCallback(() =>
+        {
+            _seq.Kill();
+        });
+    }
 
 	public void ClickUpQuitBtn()
 	{
+		SoundManager.instance.SFXPlay("ui", uiAudioClip);
+
 		_seq = DOTween.Sequence();
 		Debug.Log("Up");
 		_seq.Join(quitBtnTransform.DOScale(new Vector3(1f, 1f, 1f), .3f).SetEase(Ease.OutBounce));
-		_seq.Join(backgroundImg.DOFade(0, 0.7f));
 		backgroundImg.enabled = false;
-		_seq.Join(settingPanel.DOScale(Vector3.zero, 0.7f)).SetEase(Ease.Linear).OnComplete(()=>
-		{
-			_seq.Kill();
-
-		});
-	}
-
-	#endregion
-
-
-	#region VolumeBtn
-	public void ClickUpVolumeBtn()
-	{
-		_seq = DOTween.Sequence();
-		Debug.Log("Up");
-		_seq.Join(volumeBtnTransform.DOScale(new Vector3(4f, 4f, 4f), .3f).SetEase(Ease.OutBounce));
+		_seq.Join(settingPanel.DOScale(new Vector3(0,0,0), 0.2f)).SetEase(Ease.Linear);
+		_seq.Join(backgroundImg.DOFade(0, 0.2f));
 		_seq.AppendCallback(() =>
 		{
 			_seq.Kill();
 		});
 	}
 
-	public void ClickDownVolumeBtn()
+    #endregion
+
+
+    #region VolumeBtn
+
+    #region Effect
+    public void ClickUpEffectVolumeBtn()
 	{
-		_seq = DOTween.Sequence();
+        SoundManager.instance.SFXPlay("ui", uiAudioClip);
+
+        _seq = DOTween.Sequence();
 		Debug.Log("Up");
-		_seq.Join(volumeBtnTransform.DOScale(new Vector3(3f, 3f, 3f), .3f).SetEase(Ease.OutBounce));
+		_vibeIndex = _vibeIndex == 1 ? 0 : 1;
+        vibeObject.GetComponent<Image>().sprite = VibeImages[_vibeIndex].sprite;
+        _seq.Append(effectVolumeSound.DOScale(new Vector3(4f, 4f, 4f), .3f).SetEase(Ease.OutBounce));
 		_seq.AppendCallback(() =>
 		{
 			_seq.Kill();
 		});
 	}
-	#endregion
-	public void RestartGame()
+
+	public void ClickDownEffectVolumeBtn()
 	{
-		SceneManager.LoadScene(1);
+		_seq = DOTween.Sequence();
+		Debug.Log("Up");
+		_seq.Append(effectVolumeSound.DOScale(new Vector3(3f, 3f, 3f), .3f).SetEase(Ease.OutBounce));
+		_seq.AppendCallback(() =>
+		{
+			_seq.Kill();
+		});
+	}
+    #endregion
+
+    #region BGM
+    public void ClickUpBGMVolumeBtn()
+    {
+        _seq = DOTween.Sequence();
+        Debug.Log("Up");
+        _seq.Join(bgmVolumeSound.DOScale(new Vector3(4f, 4f, 4f), .3f).SetEase(Ease.OutBounce));
+        _seq.AppendCallback(() =>
+        {
+            _seq.Kill();
+        });
+    }
+
+    public void ClickDownBGMVolumeBtn()
+    {
+        _seq = DOTween.Sequence();
+        Debug.Log("Up");
+        _seq.Join(bgmVolumeSound.DOScale(new Vector3(3f, 3f, 3f), .3f).SetEase(Ease.OutBounce));
+        _seq.AppendCallback(() =>
+        {
+            _seq.Kill();
+        });
+    }
+
+
+    #endregion
+    #endregion
+    public void RestartGame()
+	{
+        SoundManager.instance.SFXPlay("ui", uiAudioClip);
+
+        SceneManager.LoadScene(1);
 	}
 }
