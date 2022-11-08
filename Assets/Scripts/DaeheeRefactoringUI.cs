1445da18c4298b38e6663e96243b83759838fde8
Daehee Kang <daeheuigang@gmail.com>
2022-11-08 23:32:30 +0900

    ui refactoring

--- Assets/Scripts/DaeheeRefactoringUI.cs
+++ Assets/Scripts/DaeheeRefactoringUI.cs
@@ -34,7 +34,7 @@ public class DaeheeRefactoringUI : MonoBehaviour
     {
         seq = DOTween.Sequence();
 
-        ButtonClickEffect(_storeButton, true, false);
+        ButtonClickEffect(_storeButton /*true*//*, false*/);
         seq.AppendInterval(0.5f);
         seq.Append(_storeBackGround.transform.DOMoveY(2560 / 2, 0.26f));
         seq.AppendCallback(() =>
@@ -45,7 +45,7 @@ public class DaeheeRefactoringUI : MonoBehaviour
 
     private void CloseStoreUI()
     {
-        ButtonClickEffect(_storeBackButton, false, true);
+        ButtonClickEffect(_storeBackButton /*false*//*, true*/);
         _storeBackGround.transform.DOMoveY(-2560 / 2, 0.34f);
     }
 
@@ -53,14 +53,14 @@ public class DaeheeRefactoringUI : MonoBehaviour
     {
         seq = DOTween.Sequence();
 
-        ButtonClickEffect(_settingButton, false, false);
+        ButtonClickEffect(_settingButton /*false*//*, false*/);
         seq.AppendInterval(0.1f);
         seq.Append(_settingBackGround.transform.DOMoveY(2560 / 2, 0.15f));
     }
 
     private void CloseSetting()
     {
-        ButtonClickEffect(_settingBackButton, false, true);
+        ButtonClickEffect(_settingBackButton /*false*//*, true*/);
         _settingBackGround.transform.DOMoveY(2560 + (2560 / 2), 0.34f);
     }
 
@@ -69,14 +69,14 @@ public class DaeheeRefactoringUI : MonoBehaviour
     /// </summary>
     /// <param name="button"></param>
     /// <param name="isFade"></param>
-    private void ButtonClickEffect(Button button, bool isFade = false, bool isAble = true)
+    private void ButtonClickEffect(Button button/*, bool isFade = false*//*, bool isAble = true*/)
     {
         seq = DOTween.Sequence();
 
         seq.Append(button.transform.DOScale(1.8f, 0.1f));
         seq.Append(button.transform.DOScale(2f, 0.05f));
 
-        int fadevalue = isFade ? 0 : 1;
+       // int fadevalue = isFade ? 0 : 1;
 
         for (int i = 0; i < _endImageList.Count; i++)
         {
@@ -84,21 +84,17 @@ public class DaeheeRefactoringUI : MonoBehaviour
             if (b != null)
             {
                 Debug.Log(b.name);
-                if (b.name == "GoBackButton")
-                {
-                    Debug.Log(isFade);
-                    Debug.Log(isAble);
-                }
-
-                seq.AppendCallback(() => b.interactable = isAble);
+                //seq.AppendCallback(() => b.interactable = isAble);
             }
         }
 
-        for (int i = 0; i < _endImageList.Count; i++)
-        {
-            seq.Append(_endImageList[i].GetComponent<Image>()?.DOFade(fadevalue, 0.05f));
-            seq.Append(_endImageList[i].GetComponent<TextMeshProUGUI>()?.DOFade(fadevalue, 0.05f));
-        }
+        //for (int i = 0; i < _endImageList.Count; i++)
+        //{
+        //    seq.Append(_endImageList[i].GetComponent<Image>()?.DOFade(fadevalue, 0.05f));
+        //    seq.Append(_endImageList[i].GetComponent<TextMeshProUGUI>()?.DOFade(fadevalue, 0.05f));
+        //}
+
+
         //seq.AppendCallback(()=>
         //{
         //seq.Kill();
